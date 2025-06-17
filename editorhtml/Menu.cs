279e8354a6077ceac88f2623d889bb5d2c76d756
andfb <anderson.pereira@fieb.edu.br>
2025-06-17 12:30:09 -0300

    Add files via upload

---
+++ editorhtml/Menu.cs
@@ -0,0 +1,79 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOptions(option);
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 38; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 38; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 38; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Arquivo novo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir arquivo");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção escolhida: ");
+        }
+
+        public static void HandleMenuOptions(short op)
+        {
+            switch (op)
+            {
+                case 1: Editor.Show(); break;
+                case 2: Viewer.Show(""); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0); break;
+                    }
+                default: Show(); break;
+            }
+        }
+    }
+}
